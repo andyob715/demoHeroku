3134eb56a66f1d2a345740b8865cf7e351260fdf
Andy O'Bryan <andrew.obryan@gmail.com>
2021-11-30 09:30:55 -0500

    whole lotta updates

--- app.py
+++ app.py
@@ -2,6 +2,8 @@
 from flask import Flask, render_template, request
 import pandas as pd 
 import pickle
+import json
+import math
 
 #IMPORT ALL THE THINGS
 from sklearn.model_selection import train_test_split
@@ -32,36 +34,61 @@ app = Flask(__name__)
 
 @app.route("/", methods=["GET", 'POST'])
 def index():
+    return render_template("index.html") 
+
+
+@app.route("/predict", methods=["GET", 'POST'])
+def predict():
         #Line below will load your machine learning model
-    Outcome = "Your Value Here!"
+    # Output = "Your Value Here!"
     
     if request.method == 'POST': 
 
-
+        # load up the model saved via Pickle
         filename = 'static/pickle/pilot_forest_model.sav'
         loaded_model = pickle.load(open(filename, 'rb'))
         loaded_scaler = pickle.load(open('static/pickle/scaler.pkl', 'rb'))
     
-        input_1=request.json.get("student_class_input")
-        input_2=request.json.get("exampleDataList2")
-        input_3=request.json.get("exampleDataList3")
-        input_4=request.json.get("daysform")
+        #grab the elements from the index page entered by user
+        input_1=request.json.get("course_input_json")
+        input_2=request.json.get("start_month_json")
+        input_3=request.json.get("classes_taken_json")
+        input_4=request.json.get("expected_days_json")
+        input_5=request.json.get("max_flight_hours")
 
+        #update value to integer
         var1 = int(input_1)
         var2 = int(input_2)
         var3 = int(input_3)
         var4 = int(input_4)
+        var5 = int(input_5)*240
         
         input_list = ["coursename", "start_month", "course_count", "e2e_course_days"]
 
         raw_user_input = pd.DataFrame([[var1,var2,var3,var4]],columns=input_list)
 
         scaled_user_input = loaded_scaler.transform(raw_user_input)
-        Prediction = loaded_model.predict(scaled_user_input)
-        # return Prediction
-        return {"Prediction": Prediction}
         
-    return render_template("index.html", Prediction=Outcome)
+        Guess = loaded_model.predict(scaled_user_input)
+
+        Max_Income = var5
+
+        Actual_Cost = Guess[0]*175
+
+        Actual_Income = Guess[0]*240
+
+        if Actual_Income > Max_Income:
+            Set_Income = Max_Income
+        else: 
+            Set_Income = Actual_Income
+        
+        Net_Income = Set_Income - Actual_Cost
+
+        Income_Ratio = Net_Income / Set_Income
+
+        return {"Prediction": Guess[0],"Max_Income":Max_Income,"Actual_Income":Actual_Income,"Cost":Actual_Cost,"Net_Income":Net_Income,"Income_Pct":Income_Ratio}
+        
+    return render_template("prediction.html")
 
 if __name__ == '__main__':
     app.run(debug=True)
--- static/js/app.js
+++ static/js/app.js
@@ -1,32 +1,60 @@
+const max_income = {
+    1:30,
+    2:80,
+    3:50,
+    4:130,
+    5:20,
+    6:20,
+    7:16,
+    8:20,
+    9:20
+}
+
+
 function calchours()
 {
-    let student_class_input = document.getElementById("student_class_input").value
-    let exampleDataList2 = document.getElementById("exampleDataList2").value
-    let exampleDataList3 = document.getElementById("exampleDataList3").value
-    let daysform = document.getElementById("daysform").value
+    let course_input = document.getElementById("course_input").value
+    let start_month = document.getElementById("start_month").value
+    let classes_taken = document.getElementById("classes_taken").value
+    let expected_days = document.getElementById("expected_days").value
     
-    console.log("Student Input",student_class_input)
-    console.log("exampleDataList2",dropdown2)
-    console.log("exampleDataList3",dropdown3)
-    console.log("daysform",daysform)
+    console.log("course_input",course_input)
+    console.log("start_month",start_month)
+    console.log("classes_taken",classes_taken)
+    console.log("expected_days",expected_days)
+    
+    for (const [course, hours] of Object.entries(max_income)) {
+        if (course === course_input) {
+          var flight_hours = hours;
+          console.log(flight_hours)
+        }
+      }
+    json_obj = JSON.stringify({
+        course_input_json: course_input,
+        start_month_json: start_month,
+        classes_taken_json: classes_taken,
+        expected_days_json: expected_days,
+        max_flight_hours:flight_hours
+    })
 
-    fetch("/index", {
+    console.log(json_obj)
+
+    fetch("/predict", {
         method: "POST", 
-        body: JSON.stringify(
-            {
-            student_class_input: student_class_input,
-            input_2: exampleDataList2,
-            input_3: exampleDataList3,
-            input_4: daysform
-            }),
+        body: json_obj,
         headers:
             {
-            "Content-type":"application/json;charset=UTF-8"
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
             } 
-}).then(resp=>{
-    return resp.json()
     }).then(resp=>{
-    console.log(resp)
-    document.getElementById("prediction").innerHTML=resp.Prediction
-    console.log(resp.Prediction)}
-    )}
+        return resp.json()
+
+    }).then(resp=>{
+        console.log(resp)
+        document.getElementById("prediction").innerHTML=resp.Prediction.toFixed(2)
+        document.getElementById("income_pct").innerHTML=resp.Income_Pct.toFixed(2)
+        console.log(resp.Prediction)
+        console.log(resp.Income_Pct);
+    })
+}
